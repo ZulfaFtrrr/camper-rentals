662b0c640c283f9c6f0737d7b267a98cb27f0b9d
Lolita Yenik <lolitayenik@ukr.net>
2024-05-04 05:53:16 +0300

    update header

--- src/components/Header/Navigation/Navigation.jsx
+++ src/components/Header/Navigation/Navigation.jsx
@@ -1,28 +1,42 @@
 import { NavLink } from 'react-router-dom';
 import s from './Navigation.module.css';
+import Icon from '../../Icon/Icon';
 
 const Navigation = () => {
   return (
-    <div className={s.navWrapper}>
-      <NavLink
-        className={({ isActive }) => `${s.navLink} ${isActive ? s.active : ''}`}
-        to="/"
-      >
-        Home
-      </NavLink>
+    <div className={s.navLogoWrapper}>
+      <div className={s.navWrapper}>
+        <NavLink
+          className={({ isActive }) =>
+            `${s.navLink} ${isActive ? s.active : ''}`
+          }
+          to="/"
+        >
+          Home
+        </NavLink>
 
-      <NavLink
-        className={({ isActive }) => `${s.navLink} ${isActive ? s.active : ''}`}
-        to="/catalog"
-      >
-        Catalog
-      </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            `${s.navLink} ${isActive ? s.active : ''}`
+          }
+          to="/catalog"
+        >
+          Catalog
+        </NavLink>
 
-      <NavLink
-        className={({ isActive }) => `${s.navLink} ${isActive ? s.active : ''}`}
-        to="/favorites"
-      >
-        Favorites
+        <NavLink
+          className={({ isActive }) =>
+            `${s.navLink} ${isActive ? s.active : ''}`
+          }
+          to="/favorites"
+        >
+          Favorites
+        </NavLink>
+      </div>
+
+      <NavLink className={s.logoText} to="/">
+        <Icon id={'alcove'} size="38" fill="#ffc531" stroke="none" />
+        <p> Camper Rentals</p>
       </NavLink>
     </div>
   );
