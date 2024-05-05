ea28677ded817141ab462617af0019044e510f4b
Lolita Yenik <147347232+imlaamer@users.noreply.github.com>
2024-05-05 11:54:48 +0300

    Update email constants.js

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -4,7 +4,7 @@ export const NAME_ERROR_MESSAGE = 'Enter a valid username';
 export const REQUIRED_NAME_MESSAGE = 'Name field is required';
 
 //---------------EMAIl
-export const EMAIL_ERROR_MESSAGE = 'Enter a valid email';
+export const EMAIL_ERROR_MESSAGE = 'Email must have the format xx@xx.xx';
 export const REQUIRED_EMAIL_MESSAGE = 'Email field is required';
 
 //---------------DATE
